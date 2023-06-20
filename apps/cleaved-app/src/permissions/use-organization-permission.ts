ebf532de80ef6d80143e664292979360e1595e97
Jeremy Slottje <jeremy.slottje@gmail.com>
2023-06-20 17:11:03 -0400

    usememo example

--- apps/cleaved-app/src/permissions/use-organization-permission.ts
+++ apps/cleaved-app/src/permissions/use-organization-permission.ts
@@ -1,27 +1,15 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useMemo } from "react";
 
 import { OrganizationMembershipsContext } from "../contexts";
 import { OrgPermissionLevel } from "../generated-types/graphql";
 
 export const useOrganizationPermission = (permissionLevels: OrgPermissionLevel[]): boolean => {
-  const [hasPermission, setHasPermission] = useState(false);
-  const { organizationMembershipsData, organizationMembershipsDataLoading } =
-    useContext(OrganizationMembershipsContext);
-  const accountPermission = organizationMembershipsData && organizationMembershipsData[0].userPermissionInOrg;
+  const { organizationMembershipsData } = useContext(OrganizationMembershipsContext);
 
-  useEffect(() => {
-    let foundPermission = false;
-
-    permissionLevels.forEach((permissionLevel) => {
-      if (permissionLevel === accountPermission) {
-        foundPermission = true;
-      }
-    });
-
-    setHasPermission(foundPermission);
-  }, [accountPermission, organizationMembershipsData, organizationMembershipsDataLoading, permissionLevels]);
-
-  return hasPermission;
+  return useMemo(() => {
+    const accountPermission = organizationMembershipsData && organizationMembershipsData[0].userPermissionInOrg;
+    return permissionLevels.some((pl) => pl === accountPermission);
+  }, [organizationMembershipsData, permissionLevels]);
 };
 
 /* 
