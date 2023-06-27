45be2359a190964368719ae72a09b0fe33f58a2f
Chris Campbell <centinel3@gmail.com>
2023-06-27 10:47:55 -0400

    bundle public folder into build folder at compile time

--- apps/cleaved-app/webpack.prod.js
+++ apps/cleaved-app/webpack.prod.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
   // devtool: "source-map", // do we need this in prod?
@@ -15,6 +16,9 @@ module.exports = merge(common, {
       // "process.env.GOOGLE_TAG_MANAGER_ID": JSON.stringify("XXX-XXXXX"),
       "process.env.GRAPHQL_API_SERVICE_URL": JSON.stringify("https://api.app.cleaved.com/graphql"),
       "process.env.MEDIA_ENDPOINT": JSON.stringify("https://api.app.cleaved.com"),
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "public" }],
     }),
   ],
 });
